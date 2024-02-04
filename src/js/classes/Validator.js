2c910f1953a6c5b70150937a0827e66e03b1b48a
cpb-home <cpb@bizb.ru>
2024-02-04 17:46:50 +0300

    test done

--- src/js/classes/Validator.js
+++ src/js/classes/Validator.js
@@ -2,9 +2,9 @@ export default class Validator {
   static validateUsername(name) {
     
     if (name.match(/^(?!.*[\d]{4})[a-zA-Z]{1}[a-zA-Z-_0-9]+[a-zA-Z]{1}$/) === null) {
-      return `bad login ${name}`;
+      return `bad login`;
     } 
 
-    return `login accessed ${name}`;  
+    return `login accessed`;  
   }
 }
--- src/js/classes/__tests__/Validator.test.js
+++ src/js/classes/__tests__/Validator.test.js
@@ -1,13 +1,37 @@
 import Validator from "../Validator";
 
 test("right login", () => {
-  const result = Validator.validateUsername("aaBB88-_");
+  const result = Validator.validateUsername("aaBB88-fdgew");
 
   expect(result).toBe("login accessed");
 });
 
-test("wrong login", () => {
+test("wrong login _ in the end", () => {
   const result = Validator.validateUsername("aaBB.88-_");
+
+  expect(result).toBe("bad login");
+});
+
+test("wrong login digit in the end", () => {
+  const result = Validator.validateUsername("aaBB.88-3");
+
+  expect(result).toBe("bad login");
+});
+
+test("wrong login digit in the begining", () => {
+  const result = Validator.validateUsername("3aaBB.88-f");
+
+  expect(result).toBe("bad login");
+});
+
+test("wrong login - in the begining", () => {
+  const result = Validator.validateUsername("-aaBB.88-k");
+
+  expect(result).toBe("bad login");
+});
+
+test("wrong login 4 digits", () => {
+  const result = Validator.validateUsername("aa4567BB.88-_");
 
   expect(result).toBe("bad login");
 });
